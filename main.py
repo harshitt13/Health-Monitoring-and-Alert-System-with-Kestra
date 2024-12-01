d31f366247a5d486cf42839c8145d29b296d5f76
harshitt13 <hk9084760388@gmail.com>
2024-12-01 10:38:09 +0530

    unchanged

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from data_processing import generate_mock_data
 from randomforest import train_model, load_model, predict_critical_levels
 from sms_alert import send_sms_alert
 
-# Step 1: Generate mock data
+# Generate mock data
 data = generate_mock_data()
 
-# Step 2: Train the model (only run this once or when retraining is needed)
-train_model(data)
+# Train the model (only run this once or when retraining is needed)
+# train_model(data)
 
-# Step 3: Load the trained model
+# Load the trained model
 model = load_model()
 
-# Step 4: Simulate real-time monitoring
+# Simulate real-time monitoring
 for index, row in data.iterrows():
     heart_rate = row['heart_rate']
     glucose_level = row['glucose_level']
